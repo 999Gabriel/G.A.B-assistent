66d0ce4254ec7b1e026c329bac008956ad5e8127
999Gabriel <999gabriel.winkler@gmail.com>
2025-01-14 10:55:42 +0100

    Initial Commit

---
+++ Jarvis/JarvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  JarvisApp.swift
+//  Jarvis
+//
+//  Created by Gabriel Winkler on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct JarvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JarvisTests/JarvisTests.swift
@@ -0,0 +1,16 @@
+//
+//  JarvisTests.swift
+//  JarvisTests
+//
+//  Created by Gabriel Winkler on 1/14/25.
+//
+
+import Testing
+
+struct JarvisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
